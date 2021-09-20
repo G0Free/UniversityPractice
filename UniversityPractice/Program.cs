6d542cb52dd358ada8d4e68aab44a2240cf2a100
G0Free <gyorfi.bence@gmail.com>
2021-09-20 19:17:49 +0200

    Add project files.

---
+++ UniversityPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
